4b6fbf7969955fd0e7b5d72f7dff9ec1f07ecf93
rwalerow <robert.walerowicz@softwaremill.com>
2019-04-04 18:29:18 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Hi there Robert
           </a>
         </header>
       </div>
